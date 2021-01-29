463e7173e45232447515bc7689f15ea9bfb84666
Albi Ummid <albi.ummid@gmail.com>
2021-01-29 16:10:43 +0600

    added email and password verfier

---
+++ test.js
@@ -0,0 +1,17 @@
+
+
+ 
+
+    const email = ["albi-meem@gmail.com","meem-ummid@gmail.com"]
+    const emailInput = document.getElementById("email").innerText;
+    for (let i = 0; i < email.length; i++) {
+        const existingEmail = email[i];
+        console.log(existingEmail);
+        // if(existingEmail = emailInput){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
+    }
+    // return existingEmail;
